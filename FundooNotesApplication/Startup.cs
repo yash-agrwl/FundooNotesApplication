3a4c1c4f5ea914f87061c19a2313e156da0ec4ac
yash-agrwl <agarwalyash7012@gmail.com>
2022-06-11 08:30:10 +0530

    added some packages to remove error and perform migration

--- FundooNotesApplication/Startup.cs
+++ FundooNotesApplication/Startup.cs
@@ -2,6 +2,7 @@ using BusinessLayer.Interface;
 using BusinessLayer.Manager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
