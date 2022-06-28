e57e9e9378838fcbb10a799c237a463c1e89b459
yash-agrwl <agarwalyash7012@gmail.com>
2022-06-28 11:25:39 +0530

    added collaborator model

---
+++ CommonLayer/CollaboratorModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace CommonLayer
+{
+    public class CollaboratorModel
+    {
+        [Key]
+        public int CollaborationId { get; set; }
+
+        [ForeignKey("Notes")]
+        public int NoteId { get; set; }
+
+        [JsonIgnore]
+        public NotesModel Notes { get; set; }
+
+        public string SharedEmail { get; set; }
+    }
+}
